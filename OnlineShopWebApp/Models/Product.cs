59ee5193a7bbde952b522db0e48f4dacc398c912
Magomedali Gajiev <mag198421@gmail.com>
2024-07-29 00:20:10 +0300

    Добавил класс Product

---
+++ OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal Cost { get; }
+        public string Description { get; }
+
+        public Product(string name, decimal cost, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}\n{Name}\n{Cost}";
+        }
+    }
+}
