e639d6056ac952c5a03b4e1fdf3a6b494e632d69
Magomedali Gajiev <mag198421@gmail.com>
2024-07-28 22:58:17 +0300

    Задание 4

---
+++ OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case null:
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                case "/": return b != 0 ? $"{a} / {b} = {a / b}" : "Нельзя делить на ноль!";
+                default: return "Операция может мыть толькр +, -, *, /";
+            }
+        }
+    }
+}
