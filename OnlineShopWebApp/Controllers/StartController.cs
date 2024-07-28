36cb3860856907ea47a658490f7bfec54d6fab72
Magomedali Gajiev <mag198421@gmail.com>
2024-07-28 05:42:58 +0300

    Задание 1

---
+++ OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var currentHour = DateTime.Now.Hour;
+
+            if (currentHour >= 0 && currentHour <= 6)
+            {
+                return "Доброй ночи";
+            }
+            if (currentHour > 6 && currentHour <= 11)
+            {
+                return "Доброе утро";
+            }
+            if (currentHour > 11 && currentHour <= 17)
+            {
+                return "Добрый день";
+            }
+            return "Доброй ночи";
+        }
+    }
+}
